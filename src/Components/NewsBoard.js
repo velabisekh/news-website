660905eeddcae41b16b18e48118717223f9747a6
velabisekh <148607755+velabisekh@users.noreply.github.com>
2024-06-12 04:29:42 +0530

    Update NewsBoard.js

--- src/Components/NewsBoard.js
+++ src/Components/NewsBoard.js
@@ -5,13 +5,13 @@ const NewsBoard = ({category}) => {
     const [articles,setArticles] = useState([]);
 
     useEffect(()=>{
-      let url = `https://newsapi.org/v2/everything?q=india&apiKey=${process.env.REACT_APP_API_KEY}`;
+      let url = `https://newsapi.org/v2/everything?q=india&apiKey=ecfaf9eaaa8d40a5b5d769210f5ee616`;
       fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
 
   },[])
 
     useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${process.env.REACT_APP_API_KEY}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=ecfaf9eaaa8d40a5b5d769210f5ee616`;
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
 
     },[category])
